7a784dfe7b4dedc09ca092536fe3d7a48c7402a5
linc <3288235@qq.com>
2019-06-09 17:35:12 +0800

    [UPDATE] add find functions

--- src/IndexManger/BplusTree.hpp
+++ src/IndexManger/BplusTree.hpp
@@ -12,6 +12,9 @@
 #include "../Type/Value.hpp"
 
 #define GETVAL(x) x
+#define NEXT_NODE(x) (x->foffset[NODE_SIZE])
+#define READ_NODE_BUF(x, y) x->setoffset(0); x->read(y, NODE_SIZE)
+#define GET_IDX_BLOCK(x) bufferManager->getblock(MakeID(fileManager, x))
 // a index key and a struct to find the record in the table file
 
 const int NODE_SIZE = (BLOCK_SIZE - 2 * sizeof(short) - sizeof(long long)) / (sizeof(long long) + sizeof(long long));
@@ -35,7 +38,7 @@ class BplusTree {
     std::vector<int> stackPos;
     // External dependencies
     // RecordPtr recordManager
-    FilePtr fileManager;
+    FilePtr fileManager; // for the index file
     BufferPtr bufferManager;
 
 public:
@@ -70,19 +73,17 @@ public:
             if (valcmp(GETVAL(nodeBuffer->roffset[nodeBuffer->size - 1]), v) <= 0) {
                 // val <= v
                 stackPos.push_back(nodeBuffer->size);
-                blk = bufferManager->getblock(MakeID(fileManager, nodeBuffer->roffset[size]));
-                blk->setoffset(0);
-                blk->read(nodeBuffer, NODE_SIZE);
+                blk = bufferManager->getblock(MakeID(fileManager, nodeBuffer->foffset[size]));
+                READ_NODE_BUF(blk, nodeBuffer);
                 continue;
             }
             for (int i; i < nodeBuffer->size; i++) {
                 if (valcmp(GETVAL(nodeBuffer->roffset[i].i, v)) > 0) {
                     // val > v
                     stackPos.push_back(i);
-                    blk = bufferManager->getblock(MakeID(fileManager, nodeBuffer->roffset[i]));
+                    blk = bufferManager->getblock(MakeID(fileManager, nodeBuffer->foffset[i]));
                     // update node buffer
-                    blk->setoffset(0);
-                    blk->read(nodeBuffer, NODE_SIZE);
+                    READ_NODE_BUF(blk, nodeBuffer);
                     break;
                 }
             }
@@ -93,8 +94,83 @@ public:
     /* ---------- USED BY QUERY ------------ */
     long long findOne(Value v) {
         BlockPtr blk = findNode(v);
+        NodePT nodeBuffer;
+        blk->setoffset(0);
+        blk->read(nodeBuffer, NODE_SIZE);
+        for (int i = 0; i < nodeBuffer->size; i++) {
+            if (0 == valcmp(GETVAL(nodeBuffer->roffset[i]), v)) {
+                return nodeBuffer->roffset[i];
+            }
+        }
+        return 0;
+    }
+
+    BlockPtr findLeftMostNode() {
+        BlockPtr blk = root;
+        NodePT nodeBuffer;
+        READ_NODE_BUF(blk, nodeBuffer);
+        while (!nodeBuffer->isLeaf) {
+            blk = GET_IDX_BLOCK(nodeBuffer->foffset[0]);
+            READ_NODE_BUF(blk, nodeBuffer);
+        }
+        return blk;
+    }
+
+    // withLeft? leftEqual? leftValue
+    int findByRange(
+            bool wl, bool leq, Value l,
+            bool wr, bool req, Value r,
+            BpRangeConsumer consumer
+    ) {
+        int count = 0;
+        Block blk;
+        NodePT nodeBuffer;
+        int now = 0;
+        if (wl) {
+            blk = findNode(l);
+            READ_NODE_BUF(blk, nodeBuffer);
+            bool find = false;
+            // find? the lower bound
+            for (int i = o; i < nodeBuffer->size; i++) {
+                if (valcmp(GETVAL(nodeBuffer->roffset[i]), l) > 0
+                    || (valcmp(GETVAL(nodeBuffer->roffset[i]), l) == 0 && leq)) {
+                    // val > l
+                    find = true;
+                    now = i;
+                    break;
+                }
+            }
+            if (!find) {
+                if (0 == NEXT_NODE(nodeBuffer)) return 0;
+                blk = GET_IDX_BLOCK(NEXT_NODE(nodeBuffer)));
+                READ_NODE_BUF(blk, nodeBuffer);
+            }
+        } else { // !wl
+            blk = findLeftMostNode();
+            READ_NODE_BUF(blk, nodeBuffer);
+        }
+        while (true) {
+            for (int i = now; i < nodeBuffer->size; i++) {
+                if (wr && (valcmp(GETVAL(nodeBuffer->roffset[i]), r) > 0
+                           || (valcmp(GETVAL(nodeBuffer->roffset[i]), r) == 0 && !req))) {
+                    return count;
+                }
+                consumer(count++, nodeBuffer->foffset[i]);
+            }
+        }
+    }
+    // IMPORTANT used to insert new value to B+ Tree
+    bool insert(Value v, long long foffset) {
+        BlockPtr blk = findNode(v);
+        NodePT nodeBuffer;
+        READ_NODE_BUF(blk, nodeBuffer);
+        if (nodeBuffer->size < NODE_SIZE) {
+            // the node is not full, just insert
+
+        }
     }
 };
 
+using BpRangeConsumer = std::function<void(int, long long)>; // consume a count and the file offset
 
 #endif // MINISQL_B_PLUS_TREE_H
--- src/Type/Value.hpp
+++ src/Type/Value.hpp
@@ -5,31 +5,10 @@
 #ifndef MINISQL_VALUE_HPP
 #define MINISQL_VALUE_HPP
 
-class Value {
-public:
-    Type type;
-    void *ptr;
-    unsigned char charlen;
-    virtual int size(){};
-};
-
-class IntValue : public Value {
-    Value::type = Type::INT;
-
-    IntValue(int *ptr_) : Value::ptr(ptr_) {}
-};
-
-class FloatValue : public Value {
-    Value::type = Type::FLOAT;
-
-    FloatValue(float *ptr_) : Value::ptr(ptr_) {}
-};
-
-class CharValue : public Value {
-    Value::type = Type::CHAR;
-
-    CharValue(char *ptr_) : Value::ptr(ptr_) {}
-};
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include "../utils/ErrorManager.hpp"
 
 enum class Type {
     INT,
@@ -37,9 +16,38 @@ enum class Type {
     CHAR
 };
 
+class Value {
+public:
+    Type type;
+    void *ptr;
+    unsigned char charlen;
+};
+
+class IntValue : public Value {
+    IntValue(int *ptr_) {
+        this->type = Type::INT;
+        this->ptr = ptr_;
+    }
+};
+
+class FloatValue : public Value {
+    FloatValue(float *ptr_) {
+        this->type = Type::FLOAT;
+        this->ptr = ptr_;
+    }
+};
+
+class CharValue : public Value {
+    CharValue(char *ptr_, unsigned char len_) {
+        this->type = Type::CHAR;
+        this->ptr = ptr_;
+        this->charlen = len_;
+    }
+};
+
 static int valcmp(Value a, Value b) {
     if (a.type != b.type) {
-        std::cout << "[ERROR] Comparing incompatible type" << std::endl;
+        throw TypeError("[ERROR] Comparing incompatible type");
     } else {
         switch (a.type) {
             case Type::INT:
@@ -47,7 +55,7 @@ static int valcmp(Value a, Value b) {
             case Type::FLOAT:
                 return *((float *) a.ptr) > *((float *) b.ptr) ? 1 : (*((float *) a.ptr) < *((float *) b.ptr) ? -1 : 0);
             case Type::CHAR:
-                return strncmp((char *)a.ptr, (char *)b.ptr, a.);
+                return strcmp((char *)a.ptr, (char *)b.ptr);
         }
     }
 }
